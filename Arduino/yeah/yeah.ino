25986481899822d3e474f0b8eac01509dc743906
Nik Floden <nfloden@gmail.com>
2017-11-05 13:05:27 -0700

    /

--- Arduino/yeah/yeah.ino
+++ Arduino/yeah/yeah.ino
@@ -128,24 +128,7 @@ void startAdv(void)
 
 void loop()
 {
-  // Forward data from HW Serial to BLEUART
-  while (Serial.available())
-  {
-    // Delay to wait for enough input, since we have a limited transmission buffer
-    delay(2);
 
-    uint8_t buf[64];
-    int count = Serial.readBytes(buf, sizeof(buf));
-    bleuart.write( buf, count );
-  }
-
-  // Forward from BLEUART to HW Serial
-  while ( bleuart.available() )
-  {
-    uint8_t ch;
-    ch = (uint8_t) bleuart.read();
-    Serial.write(ch);
-  }
   
   /* Get a new sensor event */
   sensors_event_t event, aevent, mevent;
@@ -180,6 +163,26 @@ void loop()
   prevAccel = currAccel;
   prevTime = currTime;
   Serial.print("Velocity is "); Serial.print(velocity, 4); 
+  // Forward data from HW Serial to BLEUART
+  while (Serial.available())
+  {
+    // Delay to wait for enough input, since we have a limited transmission buffer
+    delay(2);
+
+    uint8_t buf[64];
+    int count = Serial.readBytes(buf, sizeof(buf));
+    bleuart.write( buf, count );
+  }
+
+  // Forward from BLEUART to HW Serial
+  while ( bleuart.available() )
+  {
+    uint8_t ch;
+    ch = (uint8_t) bleuart.read();
+    Serial.write(ch);
+  }
+
+  
   // Request CPU to enter low-power mode until an event/interrupt occurs
   waitForEvent();
 }
